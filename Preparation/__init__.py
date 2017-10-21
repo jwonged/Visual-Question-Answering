eb313286806264f86ec9757f025c3a3ed71877b3
Joshua Wong <dsjw2@cam.ac.uk>
2017-10-21 14:21:00 +0100

    Creating BOW vector

--- Preparation/__init__.py
+++ Preparation/__init__.py
@@ -1,6 +1,16 @@
 import json
+from nltk import word_tokenize
 
 def getAllQuestionDetails(fileName):
     fileData = open(fileName)
     json_data = json.load(fileData)
     return json_data['questions']
+
+def createBOWVocab(questionSet):
+    vocabBOW = {}
+    for question in questionSet:
+        for word in word_tokenize(question):
+            if word != '?':
+                vocabBOW[word] = 0
+    return vocabBOW
+
