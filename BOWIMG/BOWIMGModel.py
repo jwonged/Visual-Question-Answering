c5e21f4427f801354e4ff9707c63f839b5021cc2
jwonged <dsjw2@cam.ac.uk>
2017-11-23 09:13:03 +0000

    test minibatch

--- BOWIMG/BOWIMGModel.py
+++ BOWIMG/BOWIMGModel.py
@@ -8,17 +8,21 @@ SemiEnd to end BOWIMG model (excluding preprocessing)
 if __name__ == "__main__":
 	#files that depend on set
 	questionFile = '/media/jwong/Transcend/VQADataset/TrainSet/Questions_Train_mscoco/Preprocessed/processedOpenEnded_trainQns.json'
-	annotationsFile = '/media/jwong/Transcend/VQADataset/TrainSet/mscoco_train_annotations.json'
 	imageFile = '/media/jwong/Transcend/VQADataset/TrainSet/ExtractedImageFeatures/VQAImgFeatures_Train.json'
 
 	#constant files
 	mostFreqAnswersFile = '/home/jwong/Documents/LinuxWorkspace/Visual-Question-Answering/resources/1000MostFreqAnswers.csv'
 	vocabBOWfile = '/home/jwong/Documents/LinuxWorkspace/Visual-Question-Answering/resources/BOWdimensions.csv'
-	
+
+	#1-25 train batches
+	miniBatchPath = '/media/jwong/Transcend/VQADataset/TrainSet/trainMiniBatches/TrainMiniBatch'
 	print('Loading files...')
-	inputProcessor = InputProcessor.InputProcessor(questionFile, vocabBOWfile, imageFile, annotationsFile, mostFreqAnswersFile)
-	xVals, yVals = inputProcessor.getXandYbatch()
+	inputProcessor = InputProcessor.InputProcessor(questionFile, vocabBOWfile, imageFile, mostFreqAnswersFile)
+	xVals, yVals = inputProcessor.getXandYbatch(miniBatchPath + str(1) + '.json')
+	xtes, ytes = inputProcessor.getXandYbatch(miniBatchPath + str(2) + '.json')
+
+	#for i in range(25):
 
 	Decider = SoftmaxLayer.SoftmaxLayer()
-	Decider.runSoftmaxLayer(xVals, yVals)
+	Decider.runSoftmaxLayer(xVals, yVals, xtes, ytes)
 	print('Done.')
--- Preprocessors/AnnotationsPreprocessor.py
+++ Preprocessors/AnnotationsPreprocessor.py
@@ -13,7 +13,10 @@
 '''
 import json
 
-def preprocessTrainAnnotations(annotationsFile, outFileName):
+def preprocessTrainAnnotations():
+	annotationsTrainFile = '/media/jwong/Transcend/VQADataset/TrainSet/mscoco_train_annotations.json'
+	outTrainFileName = '/media/jwong/Transcend/VQADataset/TrainSet/Minibatches_PreprocessedAnnotations/TrainMiniBatch'
+	
 	with open(annotationsFile) as annotFile:
 			annotList = json.load(annotFile)['annotations']
 
@@ -34,13 +37,76 @@ def preprocessTrainAnnotations(annotationsFile, outFileName):
 		if (start > len(annotList)):
 			break
 
+def preprocessValTestAnnotations():
+	valFeaturesFile = '/media/jwong/Transcend/VQADataset/ValSet/VQAImgFeatures_Val.json'
+	annotationsFile = '/media/jwong/Transcend/VQADataset/ValSet/mscoco_val2014_annotations.json'
+	testFeaturesFile = '/media/jwong/Transcend/VQADataset/ValSet/VQAImgFeatures_Test.json'
+
+	testOutPath = '/media/jwong/Transcend/VQADataset/ValSet/testMiniBatches/testMiniBatch'
+	valOutPath = '/media/jwong/Transcend/VQADataset/ValSet/valMiniBatches/valMiniBatch'
+	
+	with open(annotationsFile) as annotFile:
+		annotList = json.load(annotFile)['annotations']
+	
+	with open(valFeaturesFile) as valFile:
+		valFeatures = json.load(valFile)
+
+	with open(testFeaturesFile) as testFile:
+		testFeatures = json.load(testFile)
+
+	valAnnotList = []
+	testAnnotList = []
+	errorMessages = []
+
+	for annot in annotList:
+		img_id = str(annot['image_id'])
+		if img_id in valFeatures:
+			valAnnotList.append(annot)
+		elif img_id in testFeatures:
+			testAnnotList.append(annot)
+		else:
+			print('Error could not find ' + str(annot))
+			errorMessages.append(str(annot))
+
+	batchSet(valAnnotList, valOutPath)
+	batchSet(testAnnotList, testOutPath)
+
+	print('Items in annot: ' + str(len(annotList)))
+	print('Items in val: ' + str(len(valAnnotList)))
+	print('Items in test: ' + str(len(testAnnotList)))
+
+	errorMessages.append('Items in annot: ' + str(len(annotList)))
+	errorMessages.append('Items in val: ' + str(len(valAnnotList)))
+	errorMessages.append('Items in test: ' + str(len(testAnnotList)))
+
+	with open('Errorlog.txt', 'w') as logFile:
+		for msg in errorMessages:
+			logFile.write(msg)
+	
+def batchSet(annotList, outPath):
+	start = 0
+	end = 10000
+	counter = 1
+	while True:
+		outFile = outPath + str(counter) + '.json'
+		print('Processing: ' + str(start) + ' to ' + str(end))
+		print('Writing to file: ' + outFile)
+		with open(outFile, 'w') as jsonOut:
+			json.dump(annotList[start:end], jsonOut)
+
+		start = start + 10000
+		end = end + 10000
+		counter = counter + 1
+		
+		if (start > len(annotList)):
+			break
+
 def readCheck(outFile):
 	with open(outFile) as file:
 			annotData = json.load(file)
 
+
 if __name__ == "__main__":
-	annotationsTrainFile = '/media/jwong/Transcend/VQADataset/TrainSet/mscoco_train_annotations.json'
-	outTrainFileName = '/media/jwong/Transcend/VQADataset/TrainSet/Minibatches_PreprocessedAnnotations/TrainMiniBatch'
-	readCheck(outTrainFileName + '25.json')
+	preprocessValTestAnnotations()
 
 	
