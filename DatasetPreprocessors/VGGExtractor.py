49900409b718cde896fbdaac8f4336f9435e8733
jwonged <dsjw2@cam.ac.uk>
2018-03-05 18:02:00 +0000

    np save and gc collect

--- DatasetPreprocessors/VGGExtractor.py
+++ DatasetPreprocessors/VGGExtractor.py
@@ -89,7 +89,7 @@ def convertToFeatureVecs(inputPath, inputfile, jsonFile, outputFile, idfile):
                     idlist.append(img_id)
                     
                     # filename, array data to be saved, format, delimiter
-                    featureData = net.blobs[layer_name].data[0]
+                    featureData = net.blobs[layer_name].reshape(1,-1).data[0]
                     np.savetxt(writer, featureData, fmt='%.8g')
                     #resultJSONData[img_id] = featureData
                     msg2 = ('\nImages processed: {}\n'.format(count))
